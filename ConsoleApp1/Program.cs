f4ad03834993d940ca00d586224263964a2bf3d0
32074 <32074@>
2020-10-24 18:58:03 +0800

    ok

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -8,7 +8,7 @@ namespace ConsoleApp1
         {
             Console.WriteLine("Hello World!");
             int i = 2;
-            Console.WriteLine(i);lock
+            Console.WriteLine(i);hmp
         }
     }
 }
