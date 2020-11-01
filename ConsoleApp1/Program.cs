93c43ceb40fb6ae665c583ddb83078c87ebbf2fb
涛 胡 <329749422@qq.com>
2020-11-01 21:50:19 +0800

    一起帮登录页面，用if else实现功能

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Diagnostics.SymbolStore;
+using System.Net.Mail;
 using System.Xml.Resolvers;
 
 namespace ConsoleApp1
@@ -7,27 +9,81 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            string verification = Console.ReadLine();
-            verification = "ut91";
-            string UserName = "yizi";
-            //string password = "";
+            //            用户依次由控制台输入：验证码、用户名和密码：
 
-            if (verification != "verification") 
+            //    如果验证码输入错误，直接输出：“*验证码错误”；
+            //    如果用户名不存在，直接输出：“*用户名不存在”；
+            //    如果用户名或密码错误，输出：“*用户名或密码错误”
+            //    以上全部正确无误，输出：“恭喜！登录成功！”
+
+            //PS：验证码 / 用户名 / 密码直接预设在源代码中，输入由Console.ReadLine()完成。
+
+
+            string readLine = Console.ReadLine();
+            string verification = "ut91";
+            string username = "yezi";
+            string password = "1212";
+
+            if (verification != readLine)
             {
                 Console.WriteLine("验证码错误");
             }
             else
             {
                 Console.WriteLine("验证码正确");
+                string WriteLine=Console.ReadLine();
+                if (username != WriteLine)
+                {
+                    Console.WriteLine("用户名不存在");
+                }
+                else
+                {
+                    Console.WriteLine("用户名存在");
+                }
             }
-            if (UserName != "UserName") 
+
+
+            if (username != "yezi")
             {
-                Console.WriteLine("用户名不存在");
+                Console.WriteLine("用户名错误");
             }
             else
             {
-                Console.WriteLine("用户名存在");
+                Console.WriteLine("用户名正确");
+                string WriteLine = Console.ReadLine();
+                if (password != "1212")
+                {
+                    Console.WriteLine("密码错误");
+                }
+                else
+                {
+                    Console.WriteLine("密码正确");
+                }
+                    Console.WriteLine("恭喜！登陆成功！");
+
+
             }
+
+
+
+
+
+            //string[,] mda = new string[4, 8];
+            //mda[0, 0] = "龚廷义";
+            //mda[0, 1] = "刘伟";
+            //mda[1, 1] = "廖光银";
+            //mda[1, 2] = "周丁浩";
+            //mda[1, 3] = "李智博";
+            //mda[2, 1] = "胡涛";
+            //mda[2, 2] = "阿泰";
+            //mda[2, 3] = "邹丽";
+            //string[,] mda = {  };
+            //Console.WriteLine(mda);
+
+            //string[] time = { "2020.08.20" };
+            //Console.WriteLine(time[]);
+
+
 
 
         }
